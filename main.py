a204548046aa2f606090939a2b438bdf6890dfa6
Mariya-Skakalina <scka.maria@yandex.ru>
2020-10-10 20:03:11 +0400

    Изменения

--- main.py
+++ main.py
@@ -23,6 +23,21 @@ def send_welcome(message):
     bot.register_next_step_handler(msg, name)
 
 
+keyboards = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+keyboards_btn1 = types.KeyboardButton("16:00")
+keyboards_btn2 = types.KeyboardButton("17:00")
+keyboards_btn3 = types.KeyboardButton("18:00")
+keyboards_btn4 = types.KeyboardButton("19:00")
+keyboards_btn5 = types.KeyboardButton("20:00")
+keyboards_btn6 = types.KeyboardButton("21:00")
+keyboards_btn7 = types.KeyboardButton("22:00")
+keyboards_btn8 = types.KeyboardButton("23:00")
+keyboards_btn9 = types.KeyboardButton("Назад")
+keyboards.row(keyboards_btn1, keyboards_btn2, keyboards_btn3, keyboards_btn4)
+keyboards.row(keyboards_btn5, keyboards_btn6, keyboards_btn7, keyboards_btn8)
+keyboards.row(keyboards_btn9)
+
+
 def name(message):
     if message.text == 'Цены':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -36,26 +51,13 @@ def name(message):
         ngs = bot.send_message(message.chat.id, 'Цены на кальян', reply_markup=keyboard)
         bot.register_next_step_handler(ngs, price)
     elif message.text == "Бронирование":
-        keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        keyboard_btn1 = types.KeyboardButton("16:00")
-        keyboard_btn2 = types.KeyboardButton("17:00")
-        keyboard_btn3 = types.KeyboardButton("18:00")
-        keyboard_btn4 = types.KeyboardButton("19:00")
-        keyboard_btn5 = types.KeyboardButton("20:00")
-        keyboard_btn6 = types.KeyboardButton("21:00")
-        keyboard_btn7 = types.KeyboardButton("22:00")
-        keyboard_btn8 = types.KeyboardButton("23:00")
-        keyboard_btn9 = types.KeyboardButton("Назад")
-        keyboard.row(keyboard_btn1, keyboard_btn2, keyboard_btn3, keyboard_btn4)
-        keyboard.row(keyboard_btn5, keyboard_btn6, keyboard_btn7, keyboard_btn8)
-        keyboard.row(keyboard_btn9)
-        ngs = bot.send_message(message.chat.id, 'Выберите время:', reply_markup=keyboard)
+        ngs = bot.send_message(message.chat.id, 'Выберите время:', reply_markup=keyboards)
         bot.register_next_step_handler(ngs, booking)
     elif message.text == "Позвать кальянщика":
         ngs = bot.send_message(message.chat.id, "Кальянщик придет к вам как только освободится")
         bot.register_next_step_handler(ngs, name)
     elif message.text == "Назад":
-        msg = bot.send_message(message.chat.id, "нащад", reply_markup=source_markup)
+        msg = bot.send_message(message.chat.id, "Назад", reply_markup=source_markup)
         bot.register_next_step_handler(msg, send_welcome)
 
 
@@ -109,8 +111,8 @@ def booking(mes):
         ngs = bot.send_message(mes.chat.id, 'Выберите стол:', reply_markup=keyboard)
         bot.register_next_step_handler(ngs, table)
     elif mes.text == "Назад":
-        ngs = bot.send_message(mes.chat.id, "Назад")
-        bot.register_next_step_handler(ngs, send_welcome)
+        ngs = bot.send_message(mes.chat.id, "Назад", reply_markup=source_markup)
+        bot.register_next_step_handler(ngs, name('Бронирование'))
 
 
 def table(message):
