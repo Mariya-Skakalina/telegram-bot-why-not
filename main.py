558e691022a1742c91f1cefcaa5b1ead51bb876d
Mariya-Skakalina <scka.maria@yandex.ru>
2020-10-14 01:10:34 +0400

    Запись данныхd

--- main.py
+++ main.py
@@ -92,7 +92,8 @@ def booking(mes):
     if (mes.text == "16:00" or mes.text == "17:00" or mes.text == "17:00" or mes.text == "18:00"
             or mes.text == "19:00" or mes.text == "20:00" or mes.text == "21:00" or mes.text == "22:00"
             or mes.text == "23:00"):
-        booking_seat['table'] = mes.text
+        booking_seat['time'] = mes.text
+        print(booking_seat['time'])
         ngs = bot.send_message(mes.chat.id, 'Выберите стол:', reply_markup=keyboards(['1', '2', '3'],
                                                                                      ['4', '5 cтол с приставкой', '6']))
         bot.register_next_step_handler(ngs, table)
