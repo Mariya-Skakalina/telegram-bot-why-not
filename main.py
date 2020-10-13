8d1c419d8ab851f841fd6b631e585aaace979e63
Mariya-Skakalina <scka.maria@yandex.ru>
2020-10-14 01:44:57 +0400

    Запись данныхd

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def booking(mes):
 
 def table(message):
     print(message.chat.id)
-    bot.forward_message(-1001199493217, message.chat.id, "Привет")
+    bot.forward_message(-1001199493217, message.chat.id, message.message_id)
     msg = bot.send_message(message.chat.id, "Стол заброанирован", reply_markup=keyboards(['Бронирование', 'Цены'],
                                                                                          ['Позвать кальянщика']))
     bot.register_next_step_handler(msg, menu)
